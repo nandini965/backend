5f74a557206725ec4c4e664cb4aa6c67cc16cfa8
HP <nandhinigoud965@gmail.com>
2025-02-11 13:35:12 +0530

    terraform.tf

--- DbConfig.js
+++ DbConfig.js
@@ -1,7 +1,13 @@
 module.exports = Object.freeze({
-    DB_HOST : 'mysql-db',
+   DB_HOST : 'mysql-db',
+   // DB_HOST=new-project-on-expence-mysql-db-1
     DB_USER : 'expense',
     DB_PWD : 'ExpenseApp@1',
     DB_DATABASE : 'transactions'
 });
 //
+//DB_HOST=new-project-on-expence-mysql-db-1
+//DB_USER=root
+//DB_PASSWORD=your_password
+//DB_NAME=expense_db
+//DB_PORT=3306
