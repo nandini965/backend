308d2830b8efa2536e148d34984cc6001bc22d6b
HP <nandhinigoud965@gmail.com>
2025-02-11 16:29:25 +0530

    terraform.tf

--- init.sql
+++ init.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE IF NOT EXISTS transactions;
-USE transactions;
-
-CREATE TABLE IF NOT EXISTS transactions (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    amount DECIMAL(10,2) NOT NULL,
-    description TEXT NOT NULL
-);
+--CREATE DATABASE IF NOT EXISTS transactions;
+--USE transactions;
+--
+--CREATE TABLE IF NOT EXISTS transactions (
+--    id INT AUTO_INCREMENT PRIMARY KEY,
+--    amount DECIMAL(10,2) NOT NULL,
+--    description TEXT NOT NULL
+--);
